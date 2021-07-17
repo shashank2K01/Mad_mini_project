d0ef155425384195b41dd791f91b672ccb17e814
shashank <shashank28042001@gmail.com>
2021-07-17 12:02:05 +0530

    somewhat done i guess

--- app/src/main/java/com/example/mad_mini_project/apifetch.java
+++ app/src/main/java/com/example/mad_mini_project/apifetch.java
@@ -2,18 +2,135 @@ package com.example.mad_mini_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class apifetch extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+public class apifetch extends AppCompatActivity {
+    JSONArray res;
+    int counter =0;
+    JSONObject question;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apifetch);
         Bundle b = getIntent().getExtras();
         String cat = b.getString("cat");
+
         String dif = b.getString("dif");
+        TextView heading = findViewById(R.id.heading);
+        TextView questionplace = findViewById(R.id.question);
+        heading.setText("Catagory: "+cat);
         Toast.makeText(this, cat, Toast.LENGTH_SHORT).show();
+
+        String category="";
+
+        String difficulty="";
+
+
+        RequestQueue requestQueue;
+        if(cat.equals("General Knowledge") ){
+            category = "&category=9";
+        }
+        else if(cat.equals("Computer Science") ){
+            category ="&category=18";
+        }
+
+        if(dif.equals("Easy")){
+            difficulty = "&difficulty=easy";
+        }
+
+        else if(dif.equals("Medium")) {
+            difficulty = "&difficulty=medium";
+        }
+
+        else
+            difficulty ="&difficulty=hard";
+
+
+        String url = "https://opentdb.com/api.php?amount=10&type=boolean";
+        if(category.length()>0)
+            url =url + category;
+        if(difficulty.length()>0)
+            url = url +difficulty;
+        requestQueue = Volley.newRequestQueue(this);
+
+
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                res = response.optJSONArray("results");
+
+                try {
+                     question = res.getJSONObject(0);
+
+                    String Question = question.optString("question").toString();
+                    Question =Question.replace("&quot;","\"\"");
+                    questionplace.setText(Question);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                //Toast.makeText(MainActivity.this, res.toString(), Toast.LENGTH_SHORT).show();
+                //check.append(res.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("myapp", "something went wrong");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+
     }
+    public void True_clicked(View v) throws JSONException {
+        String answer = question.optString("correct_answer").toString();
+        if(answer.equals("True")){
+            TextView questionplace = findViewById(R.id.question);
+            Toast.makeText(this, "correct", Toast.LENGTH_SHORT).show();
+            counter++;
+
+            question = res.getJSONObject(counter);
+            String Question = question.optString("question").toString();
+            Question =Question.replace("&quot;","\"\"");
+            questionplace.setText(Question);
+
+            
+        }
+        else{
+            Toast.makeText(this, "wrong", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void False_clicked(View v){
+
+    }
+
+
+
+
+
+
+
 }
